e5cc6cfb2552de4665b41b8a7dfa85b99df9eacb
MOHAMED AMINE MESSAOUDI MOUSSI <amine.dev28@gmail.com>
2021-05-21 15:07:40 +0100

    id contrat

--- Centre D'affaire/AchatsLogistiquePatrimoine/ClsContrat.cs
+++ Centre D'affaire/AchatsLogistiquePatrimoine/ClsContrat.cs
@@ -8,21 +8,22 @@ namespace Centre_D_affaire.AchatsLogistiquePatrimoine
 {
     class ClsContrat
     {
-        private string ID_contrat;
+        private string iD_Contrat;
         private DateTime Date_debut;
         private DateTime Date_fin;
        
 
         public DateTime Date_debut1 { get => Date_debut; set => Date_debut = value; }
         public DateTime Date_fin1 { get => Date_fin; set => Date_fin = value; }
-        public string ID_contrat1 { get => this.ID_contrat; set => this.ID_contrat = value; }
+        public string ID_Contrat { get => iD_Contrat; set => iD_Contrat = value; }
+
 
         //----------constructeur par default--------------------
         public ClsContrat() { }
         //----------constructeur renseigné--------------------
         public ClsContrat(string idcontrat , DateTime date_debut,DateTime date_fin)
         {
-            this.ID_contrat = idcontrat;
+            this.ID_Contrat = idcontrat;
             this.Date_debut = date_debut;
             this.Date_fin = date_fin;
         }
