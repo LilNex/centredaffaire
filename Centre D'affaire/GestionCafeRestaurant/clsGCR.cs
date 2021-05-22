d75c7c13ea496c2ac0d9c1798423c38e0a866006
EL MEHDI LOUTFY <LOUTFYELMEHDI@GMAIL.COM>
2021-05-22 11:34:50 +0100

    test2

---
+++ Centre D'affaire/GestionCafeRestaurant/clsGCR.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centre_D_affaire.GestionCafeRestaurant
+{
+    class clsGCR
+    {
+    }
+}
--- Centre D'affaire/GestionCafeRestaurant/frmGCR.Designer.cs
+++ Centre D'affaire/GestionCafeRestaurant/frmGCR.Designer.cs
@@ -29,12 +29,47 @@ namespace Centre_D_affaire.GestionCafeRestaurant
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(305, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "test";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(263, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 54);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TEST2";
+            // 
+            // frmGCR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmGCR";
             this.Text = "frmGCR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
