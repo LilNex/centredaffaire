fbee50cfc5bfa7df5518284caf1c96e85ff9cac5
YASSER <YASSER@DESKTOP-CJQ7VMR>
2021-06-28 19:54:45 +0100

    y_trè

---
+++ Centre D'affaire/GestionCreche/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace Centre_D_affaire.GestionCreche
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnEnre = new System.Windows.Forms.Button();
+            this.BtnPre = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lblProM = new System.Windows.Forms.Label();
+            this.lblProP = new System.Windows.Forms.Label();
+            this.lblTeleM = new System.Windows.Forms.Label();
+            this.lblTeleP = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblTele = new System.Windows.Forms.Label();
+            this.lblV = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblp = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbln = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnEnre
+            // 
+            this.BtnEnre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEnre.Location = new System.Drawing.Point(658, 400);
+            this.BtnEnre.Name = "BtnEnre";
+            this.BtnEnre.Size = new System.Drawing.Size(101, 32);
+            this.BtnEnre.TabIndex = 39;
+            this.BtnEnre.Text = "Enregistrer";
+            this.BtnEnre.UseVisualStyleBackColor = true;
+            // 
+            // BtnPre
+            // 
+            this.BtnPre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPre.Location = new System.Drawing.Point(534, 400);
+            this.BtnPre.Name = "BtnPre";
+            this.BtnPre.Size = new System.Drawing.Size(106, 32);
+            this.BtnPre.TabIndex = 38;
+            this.BtnPre.Text = "Précédent";
+            this.BtnPre.UseVisualStyleBackColor = true;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(598, 346);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(150, 20);
+            this.textBox9.TabIndex = 37;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(223, 388);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(150, 20);
+            this.textBox8.TabIndex = 36;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(598, 257);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(150, 20);
+            this.textBox7.TabIndex = 35;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(223, 310);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(150, 20);
+            this.textBox6.TabIndex = 34;
+            // 
+            // lblProM
+            // 
+            this.lblProM.AutoSize = true;
+            this.lblProM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProM.Location = new System.Drawing.Point(42, 386);
+            this.lblProM.Name = "lblProM";
+            this.lblProM.Size = new System.Drawing.Size(161, 20);
+            this.lblProM.TabIndex = 33;
+            this.lblProM.Text = "Profession du Père";
+            // 
+            // lblProP
+            // 
+            this.lblProP.AutoSize = true;
+            this.lblProP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProP.Location = new System.Drawing.Point(424, 344);
+            this.lblProP.Name = "lblProP";
+            this.lblProP.Size = new System.Drawing.Size(161, 20);
+            this.lblProP.TabIndex = 32;
+            this.lblProP.Text = "Profession du Père";
+            // 
+            // lblTeleM
+            // 
+            this.lblTeleM.AutoSize = true;
+            this.lblTeleM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeleM.Location = new System.Drawing.Point(42, 308);
+            this.lblTeleM.Name = "lblTeleM";
+            this.lblTeleM.Size = new System.Drawing.Size(163, 20);
+            this.lblTeleM.TabIndex = 31;
+            this.lblTeleM.Text = "Telephone du Mère";
+            // 
+            // lblTeleP
+            // 
+            this.lblTeleP.AutoSize = true;
+            this.lblTeleP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeleP.Location = new System.Drawing.Point(424, 257);
+            this.lblTeleP.Name = "lblTeleP";
+            this.lblTeleP.Size = new System.Drawing.Size(160, 20);
+            this.lblTeleP.TabIndex = 30;
+            this.lblTeleP.Text = "Telephone du Père";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(223, 195);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(150, 20);
+            this.textBox5.TabIndex = 29;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(573, 142);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(175, 20);
+            this.textBox4.TabIndex = 28;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(201, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 20);
+            this.textBox3.TabIndex = 27;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(573, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 20);
+            this.textBox2.TabIndex = 26;
+            // 
+            // lblTele
+            // 
+            this.lblTele.AutoSize = true;
+            this.lblTele.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTele.Location = new System.Drawing.Point(42, 193);
+            this.lblTele.Name = "lblTele";
+            this.lblTele.Size = new System.Drawing.Size(166, 20);
+            this.lblTele.TabIndex = 25;
+            this.lblTele.Text = "Telephone Domicile";
+            // 
+            // lblV
+            // 
+            this.lblV.AutoSize = true;
+            this.lblV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblV.Location = new System.Drawing.Point(424, 142);
+            this.lblV.Name = "lblV";
+            this.lblV.Size = new System.Drawing.Size(43, 20);
+            this.lblV.TabIndex = 24;
+            this.lblV.Text = "Ville";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(42, 102);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(75, 20);
+            this.lblA.TabIndex = 23;
+            this.lblA.Text = "Adresse";
+            // 
+            // lblp
+            // 
+            this.lblp.AutoSize = true;
+            this.lblp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblp.Location = new System.Drawing.Point(424, 54);
+            this.lblp.Name = "lblp";
+            this.lblp.Size = new System.Drawing.Size(115, 20);
+            this.lblp.TabIndex = 22;
+            this.lblp.Text = "Nom de mère";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // lbln
+            // 
+            this.lbln.AutoSize = true;
+            this.lbln.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbln.Location = new System.Drawing.Point(42, 18);
+            this.lbln.Name = "lbln";
+            this.lbln.Size = new System.Drawing.Size(111, 20);
+            this.lbln.TabIndex = 20;
+            this.lbln.Text = "Nom de père";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnEnre);
+            this.Controls.Add(this.BtnPre);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.lblProM);
+            this.Controls.Add(this.lblProP);
+            this.Controls.Add(this.lblTeleM);
+            this.Controls.Add(this.lblTeleP);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblTele);
+            this.Controls.Add(this.lblV);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblp);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbln);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnEnre;
+        private System.Windows.Forms.Button BtnPre;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lblProM;
+        private System.Windows.Forms.Label lblProP;
+        private System.Windows.Forms.Label lblTeleM;
+        private System.Windows.Forms.Label lblTeleP;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblTele;
+        private System.Windows.Forms.Label lblV;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbln;
+    }
+}
