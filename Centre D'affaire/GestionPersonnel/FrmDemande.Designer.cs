014878a9b6877a8781809fea3104909a7e3104ac
MARIAM EL WAFI <ELWAFI.MARIAM@ofppt-edu.ma>
2021-06-29 21:20:29 +0100

    modifier la frm demande

--- Centre D'affaire/GestionPersonnel/FrmDemande.Designer.cs
+++ Centre D'affaire/GestionPersonnel/FrmDemande.Designer.cs
@@ -46,28 +46,34 @@ namespace Centre_D_affaire.GestionPersonnel
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(163, 73);
+            this.checkBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(112, 17);
+            this.checkBox1.Size = new System.Drawing.Size(172, 24);
             this.checkBox1.TabIndex = 1;
             this.checkBox1.Text = "Demende Congée";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.UseVisualStyleBackColor = false;
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(305, 73);
+            this.checkBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox2.Location = new System.Drawing.Point(12, 42);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(123, 17);
+            this.checkBox2.Size = new System.Drawing.Size(193, 24);
             this.checkBox2.TabIndex = 2;
             this.checkBox2.Text = "Demande Demission";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.UseVisualStyleBackColor = false;
             // 
             // FrmDemande
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 109);
+            this.ClientSize = new System.Drawing.Size(239, 82);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
