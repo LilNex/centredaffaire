24c83488ffc09255dbf59e6601f3ad184e516228
YASSER <YASSER@DESKTOP-CJQ7VMR>
2021-06-29 20:57:27 +0100

    button ajouter

--- Centre D'affaire/GestionCreche/FrmPersonnel.Designer.cs
+++ Centre D'affaire/GestionCreche/FrmPersonnel.Designer.cs
@@ -43,7 +43,7 @@ namespace Centre_D_affaire.GestionCreche
             this.txtNum = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -52,7 +52,27 @@ namespace Centre_D_affaire.GestionCreche
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtNsearch = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Modification = new System.Windows.Forms.GroupBox();
+            this.btnAppliquer = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtNewNum = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtNewAd = new System.Windows.Forms.TextBox();
+            this.txtNewEmail = new System.Windows.Forms.TextBox();
+            this.txtNewTele = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtNewPrenom = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtNewNom = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Modification.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -161,9 +181,9 @@ namespace Centre_D_affaire.GestionCreche
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(34, 122);
+            this.button1.Location = new System.Drawing.Point(665, 176);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.Size = new System.Drawing.Size(132, 23);
             this.button1.TabIndex = 12;
             this.button1.Text = "Ajouter";
             this.button1.UseVisualStyleBackColor = true;
@@ -172,31 +192,33 @@ namespace Centre_D_affaire.GestionCreche
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(165, 122);
+            this.button2.Location = new System.Drawing.Point(427, 126);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.Size = new System.Drawing.Size(89, 23);
             this.button2.TabIndex = 13;
             this.button2.Text = "Modifier";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // btnSupprimer
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(298, 122);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 23);
-            this.button3.TabIndex = 14;
-            this.button3.Text = "Supprimer";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupprimer.Location = new System.Drawing.Point(665, 126);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(132, 23);
+            this.btnSupprimer.TabIndex = 14;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(447, 122);
+            this.button4.Location = new System.Drawing.Point(548, 125);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.Size = new System.Drawing.Size(101, 23);
             this.button4.TabIndex = 15;
-            this.button4.Text = "Chercher";
+            this.button4.Text = "ReChercher";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
@@ -209,11 +231,12 @@ namespace Centre_D_affaire.GestionCreche
             this.Column4,
             this.Column5,
             this.Column6});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 170);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 277);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(637, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(637, 161);
             this.dataGridView1.TabIndex = 16;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Column1
             // 
@@ -245,15 +268,202 @@ namespace Centre_D_affaire.GestionCreche
             this.Column6.HeaderText = "Numero";
             this.Column6.Name = "Column6";
             // 
+            // txtNsearch
+            // 
+            this.txtNsearch.Location = new System.Drawing.Point(548, 178);
+            this.txtNsearch.Multiline = true;
+            this.txtNsearch.Name = "txtNsearch";
+            this.txtNsearch.Size = new System.Drawing.Size(100, 20);
+            this.txtNsearch.TabIndex = 17;
+            this.txtNsearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(587, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 16);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Numero";
+            // 
+            // Modification
+            // 
+            this.Modification.Controls.Add(this.btnAppliquer);
+            this.Modification.Controls.Add(this.label13);
+            this.Modification.Controls.Add(this.txtNewNum);
+            this.Modification.Controls.Add(this.label15);
+            this.Modification.Controls.Add(this.label14);
+            this.Modification.Controls.Add(this.label12);
+            this.Modification.Controls.Add(this.txtNewAd);
+            this.Modification.Controls.Add(this.txtNewEmail);
+            this.Modification.Controls.Add(this.txtNewTele);
+            this.Modification.Controls.Add(this.label11);
+            this.Modification.Controls.Add(this.txtNewPrenom);
+            this.Modification.Controls.Add(this.label10);
+            this.Modification.Controls.Add(this.txtNewNom);
+            this.Modification.Controls.Add(this.label9);
+            this.Modification.Location = new System.Drawing.Point(12, 113);
+            this.Modification.Name = "Modification";
+            this.Modification.Size = new System.Drawing.Size(392, 143);
+            this.Modification.TabIndex = 19;
+            this.Modification.TabStop = false;
+            this.Modification.Text = "Modification";
+            this.Modification.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnAppliquer
+            // 
+            this.btnAppliquer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAppliquer.Location = new System.Drawing.Point(143, 111);
+            this.btnAppliquer.Name = "btnAppliquer";
+            this.btnAppliquer.Size = new System.Drawing.Size(113, 32);
+            this.btnAppliquer.TabIndex = 25;
+            this.btnAppliquer.Text = "Appliquer";
+            this.btnAppliquer.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(117, 118);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 16);
+            this.label13.TabIndex = 24;
+            // 
+            // txtNewNum
+            // 
+            this.txtNewNum.Location = new System.Drawing.Point(286, 82);
+            this.txtNewNum.Name = "txtNewNum";
+            this.txtNewNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNewNum.TabIndex = 23;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(211, 82);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(62, 16);
+            this.label15.TabIndex = 23;
+            this.label15.Text = "Numero";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(207, 51);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(66, 16);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "Adresse";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(224, 19);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 16);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Email";
+            // 
+            // txtNewAd
+            // 
+            this.txtNewAd.Location = new System.Drawing.Point(286, 51);
+            this.txtNewAd.Name = "txtNewAd";
+            this.txtNewAd.Size = new System.Drawing.Size(100, 20);
+            this.txtNewAd.TabIndex = 23;
+            // 
+            // txtNewEmail
+            // 
+            this.txtNewEmail.Location = new System.Drawing.Point(286, 15);
+            this.txtNewEmail.Name = "txtNewEmail";
+            this.txtNewEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtNewEmail.TabIndex = 22;
+            // 
+            // txtNewTele
+            // 
+            this.txtNewTele.Location = new System.Drawing.Point(95, 79);
+            this.txtNewTele.Name = "txtNewTele";
+            this.txtNewTele.Size = new System.Drawing.Size(100, 20);
+            this.txtNewTele.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(6, 80);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(83, 16);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Telephone";
+            // 
+            // txtNewPrenom
+            // 
+            this.txtNewPrenom.Location = new System.Drawing.Point(95, 43);
+            this.txtNewPrenom.Name = "txtNewPrenom";
+            this.txtNewPrenom.Size = new System.Drawing.Size(100, 20);
+            this.txtNewPrenom.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(6, 44);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 16);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Prenom";
+            // 
+            // txtNewNom
+            // 
+            this.txtNewNom.Location = new System.Drawing.Point(95, 12);
+            this.txtNewNom.Name = "txtNewNom";
+            this.txtNewNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNewNom.TabIndex = 22;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(6, 16);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 16);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Nom";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(469, 157);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 16);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Numero";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(410, 176);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 21;
+            // 
             // FrmPersonnel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Modification);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtNsearch);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSupprimer);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtNum);
@@ -271,6 +481,8 @@ namespace Centre_D_affaire.GestionCreche
             this.Name = "FrmPersonnel";
             this.Text = "FrmPersonnel";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Modification.ResumeLayout(false);
+            this.Modification.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -292,7 +504,7 @@ namespace Centre_D_affaire.GestionCreche
         private System.Windows.Forms.TextBox txtNum;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnSupprimer;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
@@ -301,5 +513,24 @@ namespace Centre_D_affaire.GestionCreche
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.TextBox txtNsearch;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox Modification;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtNewAd;
+        private System.Windows.Forms.TextBox txtNewEmail;
+        private System.Windows.Forms.TextBox txtNewTele;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtNewPrenom;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtNewNom;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAppliquer;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtNewNum;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
     }
 }
--- Centre D'affaire/GestionCreche/FrmPersonnel.cs
+++ Centre D'affaire/GestionCreche/FrmPersonnel.cs
@@ -24,23 +24,114 @@ namespace Centre_D_affaire.GestionCreche
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (txtnom.Text == " " || txtprenom.Text == "" || txtEmail.Text == "" || txtAd.Text == "" || txtNum.Text == "" || txttele.Text == "")
+            {
+                MessageBox.Show("Attention!!une information est vide!!");
+            }
+            else
+            {
+                try
+                {
+                    dataGridView1.Rows.Add(txtnom.Text, txtprenom.Text, txtEmail.Text, txtAd.Text, txtNum.Text, int.Parse(txttele.Text));
+                    {
 
-            dataGridView1.Rows.Add(txtnom.Text, txtprenom.Text, txtEmail.Text, txtAd.Text, txtNum.Text, txttele.Text);
-            txtnom.Clear();
-            txtprenom.Clear();
-            txtEmail.Clear();
-            txtAd.Clear();
-            txtNum.Clear();
-            txttele.Clear();
+                        {
+                            txtprenom.Clear();
+                            txtEmail.Clear();
+                            txtAd.Clear();
+                            txtNum.Clear();
+                            txttele.Clear();
+
+                        }
+                    }
+                }
+
+                catch (FormatException)
+                {
+                    MessageBox.Show("l'age invalide");
+
+                }
+
+
+            }
 
 
         }
-        int a;
+
+
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            a = e.RowIndex;
-            DataGridView row = dataGridView1.Rows[a];
-            txtnom.Text = row.Cells[0].value.Tostring();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult res = MessageBox.Show("voullez vous vraiment supprimer ce personnel)", "confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (res == DialogResult.OK)
+                {
+                    int index = this.dataGridView1.CurrentRow.Index;
+                    this.dataGridView1.Rows.RemoveAt(index);
+                    MessageBox.Show("La suppretion avec succées");
+
+                }
+                else
+                {
+                    MessageBox.Show("La suppretion ignorées");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("DatagridView est vide");
+            }
+
+        }
+
+        // private bool button4_Click(object sender, EventArgs e)
+        // {
+
+
+        // }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           // int trouve = 0;
+           // int index;
+           //// for (int i = 0 < this.dataGridView1.Rows.Count - 1; i++)
+           //  //   if (this.dataGridView1.Rows[i].Cells[5].Value.ToString== txtNsearch.Text)
+           //     {
+           //       // index = i;
+           //        // trouve = 1;
+           //     }
+           //// if (trouve == 0)
+
+           //     MessageBox.Show(" Cette personne n'existe pas dans la liste");
+
+           // else
+           //     this.Modification.Visible = true;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
+
